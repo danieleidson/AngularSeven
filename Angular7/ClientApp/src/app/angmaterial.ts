136b873430f13e4c7512c8bb2e363eba3bb4a527
Daniel Eidson <Administrator@EC2AMAZ-2ONDRMR>
2018-12-05 20:51:49 +0000

    expansion panel

--- Angular7/ClientApp/src/app/angmaterial.ts
+++ Angular7/ClientApp/src/app/angmaterial.ts
@@ -1,9 +1,9 @@
-import { MatButtonModule, MatSliderModule, MatCardModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatIconModule, MatSelectModule } from '@angular/material';
+import { MatButtonModule, MatSliderModule, MatCardModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatIconModule, MatSelectModule, MatExpansionModule } from '@angular/material';
 import { NgModule } from '@angular/core';
  
 @NgModule({
-  imports: [MatButtonModule, MatSliderModule, MatCardModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatIconModule, MatSelectModule],
-  exports: [MatButtonModule, MatSliderModule, MatCardModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatIconModule, MatSelectModule]
+  imports: [MatButtonModule, MatSliderModule, MatCardModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatIconModule, MatSelectModule, MatExpansionModule],
+  exports: [MatButtonModule, MatSliderModule, MatCardModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatIconModule, MatSelectModule, MatExpansionModule]
 })
  
 export class AngMaterialModule {
